8ea3e17bd242bf793775d2dae2900a71adec1d6d
Mitchell Gant <mgant6@gatech.edu>
2018-12-01 19:35:43 -0500

    Initial Commit

---
+++ ServeAssist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ServeAssist
+//
+//  Created by Mitchell  Gant on 12/1/18.
+//  Copyright © 2018 mitchell gant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
